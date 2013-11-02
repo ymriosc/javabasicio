b53cf0e1a86074eb7712d7ec8a2033db10d172cb
alumno <alumno@LAB706-13>
2013-11-02 16:15:27 -0500

    guardando

---
+++ src/app/clase2/AppPOO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.clase2;
+
+/**
+ *
+ * @author alumno
+ */
+public class AppPOO {
+    public static void main(String[] args) {
+        System.out.println("HOLAAAAAA");
+    }
+}
